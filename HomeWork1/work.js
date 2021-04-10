75f2267d7ea7e31c9b49e9ba7b8e46302f7fcff5
EEDolgy <eedolgy@gmail.com>
2021-04-10 19:50:14 +0200

    Home_Work_1

---
+++ HomeWork1/work.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Tc = 40;
+var Tf = (9 / 5) * Tc + 32;
+alert(Tc + ' градусов по цельсию равно ' + Tf + ' по фарингейту');
+
+var name = 'Василий';
+var admin = name;
+alert('Админ - это ' + admin);
+
+var a = 1000 + "108";
+alert('1000 + "108" = ' + a);
+
+
